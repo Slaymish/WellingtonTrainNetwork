087ccd394ac7d545abf515c68023cf2fd589453d
Hamish <hamishapps@gmail.com>
2023-04-16 14:55:47 +1200

    added javafx to project, everything compiles now

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        // Get info from /data/lines.txt.
+        File file = new File("/Users/hamishburke/Desktop/Uni/Year 2/COMP261/Assignment 2/COMPAssignment2/src/main/data/lines.txt");
+        System.out.println("File exists: " + file.exists());
+        System.out.println("File size: " + file.length());
+
+
     }
 }
+
